de1e8d0291ca785b0480ef308d057a4e004a56ac
CodeGoat <levijohnson2008@live.co.uk>
2025-04-08 01:40:29 +0100

    Refac(Text): Support text styles
    Refactors the `Goatee.Text()` class to support bold, italic, underlined and justified text styles.
    Switches are now included in the `Goatee.Text()` class to enable style combinations.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.7] - 2025-04-08
+
+### Changes
+
+- Adds support for bold, italic, underlined and justified text. The "Goatee.Text()" class now includes switches for the new styles.
+
 ## [0.1.6] - 2025-04-07
 
 ### Added
--- src/core/Text.js
+++ src/core/Text.js
@@ -2,15 +2,38 @@ import { Element } from "./Element.js";
 
 export class Text extends Element {
   body;
+  bold;
+  italic;
+  underline;
+  className;
+  style;
 
-  constructor(body) {
+  constructor(body, {
+    bold = false,
+    italic = false,
+    underline = false,
+    className = '',
+    style = ''
+  } = {}) {
     super();
     this.body = body;
+    this.bold = bold;
+    this.italic = italic;
+    this.underline = underline;
+    this.className = className;
+    this.style = style;
   }
 
   render() {
-    return `
-            <p>${this.body}</p>
-        `.trim();
+    let content = this.body;
+
+    if (this.underline) content = `<u>${content}</u>`;
+    if (this.italic)    content = `<i>${content}</i>`;
+    if (this.bold)      content = `<b>${content}</b>`;
+
+    const classAttr = this.className ? ` class="${this.className}"` : '';
+    const styleAttr = this.style ? ` style="${this.style}"` : '';
+
+    return `<p${classAttr}${styleAttr}>${content}</p>`;
   }
 }
