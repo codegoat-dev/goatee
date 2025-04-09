eec658b71d31aa0dbeb832890fa29e2af14da9e4
CodeGoat <levijohnson2008@live.co.uk>
2025-04-09 19:28:12 +0100

    Feat(Example): Update to use types
    Updates the example application to use the new types provided by Goatee.

--- example/web/contact.js
+++ example/web/contact.js
@@ -7,16 +7,20 @@ const body = new goatee.Body();
 
 const form = new goatee.Form({ method: "post", action: "/api/contact" });
 
+form.add(new goatee.Text("Please fill out the form below."));
+
 const dropdown = new goatee.Select({ name: "language", label: "Choose a language" });
 dropdown.add(new goatee.Option("js", "JavaScript"));
 dropdown.add(new goatee.Option("cs", "C#"));
 dropdown.add(new goatee.Option("goat", "Goatee", true));
 body.add(dropdown);
 
-form.add(new goatee.Input({ name: "name", label: "Your Name" }));
-form.add(new goatee.Input({ name: "email", type: "email", label: "Your Email" }));
+form.add(new goatee.Input({ name: "name", type: goatee.Types.TEXT, label: "Your Name" }));
+form.add(new goatee.Input({ name: "email", type: goatee.Types.EMAIL, label: "Your Email" }));
+form.add(new goatee.Input({ name: "age", type: goatee.Types.NUMBER, label: "Your Age" }));
 form.add(new goatee.Textarea({ name: "message", label: "Your Message" }));
 form.add(new goatee.Button("Send Message"));
+form.add(new goatee.Button({ text: "Reset", type: goatee.Types.RESET }));
 
 body.add(form);
 
